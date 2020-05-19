7fbd8b2b6756ad903ad9deaa7557dbae842fa3f3
pursuedgold <filipipw@gmail.com>
2020-05-19 07:44:49 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 int main() {
 	int v, t, atraso, p, s1, s2, s3, resultado;
 	
-	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl << "--------------------------------" << endl << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl << "--------------------------------" << endl << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
+	cout << "Quantas compras o cliente fez no ultimo ano?"<<endl;
+	cin >>v;
+	cout << "Qual o ticket medio?" << endl;
+	cin >> t;
+	
 		
 	
 	
