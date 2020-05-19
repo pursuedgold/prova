b80cdab14e4786e4335cae06478b075f26322498
pursuedgold <filipipw@gmail.com>
2020-05-19 07:48:34 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 
 int main() {
-	int v, t, atraso, p, s1, s2, s3, resultado;
+	int v, t, atraso, s1, s2, s3, resultado;
+	char p;
 	
 	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl << "--------------------------------" << endl << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
 	cout << "Quantas compras o cliente fez no ultimo ano?"<<endl;
 	cin >>v;
-	cout << "Qual o ticket medio?" << endl;
+	cout << "Qual o ticket medio?" << endl << endl;
 	cin >> t;
-	
+	cout << "Quantas vezes o cliente atrasou o pagamento?" << endl;
+	cin >> atraso;
+	cout << "A maioria das compras foi em dinheiro, cartao, ou boleto?" << endl;
+	cin >> p;
 		
 	
 	
