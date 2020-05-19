0a21bbaf2ffdcdbbed06cc60aff11e54f5e53f99
pursuedgold <filipipw@gmail.com>
2020-05-19 07:41:39 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 
 int main() {
+	int v, t, atraso, p, s1, s2, s3, resultado;
 	
-	
+	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl << "--------------------------------" << endl << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+		
 	
 	
 	
